f02cee35b70642e29e0766f6abbd5b76bb1d9ff4
abidabrar-bracu <abid.abrar@bracu.ac.bd>
2022-11-07 13:52:47 -0500

    added script for reproducibility

--- script.sh
+++ script.sh
@@ -2,11 +2,40 @@
 source ~/.bashrc
 
 # if the `pycord` environment does not exist, create using environment.yml
-# TODO
-
-# activate the environment
-conda activate pycord
+if [ ! -d "${HOME}/anaconda3/envs/pycord" ]; then
+    conda env create -f environment.yml
+fi
 
 # pull any updates from github
+# will replace all the files except info.json
 git fetch --all
 git reset --hard origin/master
+
+
+
+# create a tmux session named discord
+session="discord"
+tmux has-session -t $session 2>/dev/null
+
+if [ $? != 0 ]; then
+    echo "Session does not exists, creating"
+    tmux new-session -d -s $session
+    # Set up your session
+else
+    echo "Session already exist, stopping any running code"
+    tmux send-keys -t $session:0 C-c
+    echo "Stopped"
+fi
+
+# change directory to where the code was submitted from
+echo "Changing Directory..."
+cur_dir=$(pwd)
+tmux send-keys -t $session:0 'cd '$cur_dir C-m
+
+# activate conda environment
+echo "Activating conda environment"
+tmux send-keys -t $session:0 'conda activate pycord' C-m
+
+# run the code
+echo "Starting code"
+tmux send-keys -t $session:0 'python main.py' C-m
