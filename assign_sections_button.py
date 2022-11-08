bd53358441f81d037fbb8f15516a521c95f7aa94
Shadman Shahriar <67824850+shs-cse@users.noreply.github.com>
2022-11-08 16:44:56 +0600

    remove commented codes

--- assign_sections_button.py
+++ assign_sections_button.py
@@ -43,29 +43,6 @@ class AssignSectionsButton(Button):
                 ctype_roles = [vars.sec_roles[sec][ctype] for sec in ctype_sections]
                 all_ctype_roles = "\u200b" if not ctype_roles else ", ".join(r.mention for r in ctype_roles)
                 embed.add_field(name=f"{ctype.title()} Sections", value=all_ctype_roles)
-                # ctype_role_names = [role.name for role in ctype_roles]
                 await user.add_roles(*ctype_roles)
-            
-            # # add theory section roles, if any
-            # filt = vars.df_routine["Theory Teacher"].str.contains(initial)
-            # theory_sections = vars.df_routine.loc[filt, "Section"].values
-            # theory_roles = [vars.sec_roles[sec]['theory'] for sec in theory_sections]
-            # theory_role_names = [role.name for role in theory_roles]
-            # await user.add_roles(*theory_roles)
 
-            # # add lab section roles, if any
-            # filt = vars.df_routine["Lab Teacher"].str.contains(initial)
-            # lab_sections = vars.df_routine.loc[filt, "Section"].values
-            # lab_roles = [vars.sec_roles[sec]['lab'] for sec in lab_sections]
-            # lab_role_names = [role.name for role in lab_roles]
-            # await user.add_roles(*lab_roles)
-
-
-            # embed = discord.Embed(title=":white_check_mark: Success", color=discord.Color.green())
-            # embed.description = f"You have been given the following sections:\n"
-            # all_theory_role_name = "\u200b" if theory_role_names == [] else ", ".join(theory_role_names)
-            # all_lab_role_name = "\u200b" if lab_role_names == [] else ", ".join(lab_role_names)
-
-            # embed.add_field(name="Theory Sections", value=all_theory_role_name)
-            # embed.add_field(name="Lab Sections", value=all_lab_role_name)
             await interaction.response.send_message(embed=embed, ephemeral=True)
