6687ce7000eff7a776f6a1fed228167e5f9b423f
Shadman Shahriar <67824850+shs-cse@users.noreply.github.com>
2023-06-03 17:38:52 +0600

    switch to `marks-new` branch (#3)
    * added `fetch_marks`
    
    * ignore file `json_passed`
    
    * remove print
    
    * added children marks
    
    * change `formula` to inner function
    
    * added `marks_categories` for efficiency
    
    * added `marks_categories` for efficiency
    
    * remove old marks code
    
    * fix `marks_ids`  in `get_links`
    
    * add new line in `get-links`
    
    * huge marks update
    
    * complete marks fetch by faculty
    
    * button to show marks
    
    * Update info.json
    
    * separate theory and lab faculty roles
    
    * add new role to vars
    
    * fix bug theory and lab faculty role assigning
    
    * message strings in `literals.py`
    
    * touch up
    
    * auto revive buttons

--- assign_sections_button.py
+++ assign_sections_button.py
@@ -1,9 +1,14 @@
 # pylint: disable=unsubscriptable-object
-import discord, re, vars, literals
-from discord.ui import Button, View  #upm package(py-cord)
+import discord
+import re
+import vars
+import literals
+from discord.ui import Button, View  # upm package(py-cord)
 
 # ui elements
-class AssignSectionsView(View):
+
+
+class AssignSectionsButtonView(View):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.timeout = None
@@ -24,30 +29,41 @@ class AssignSectionsButton(Button):
         await interaction.followup.send(embeds=[embed], ephemeral=True)
 
 
-
-async def assign_sections(user):
-    if vars.faculty_role not in user.roles:
-        embed = discord.Embed(title=":x: Error", description="You are not a faculty member", color=discord.Color.red())
+async def assign_sections(member):
+    if vars.faculty_role not in member.roles:
+        embed = discord.Embed(
+            title=":x: Error", description="You are not a faculty member", color=discord.Color.red())
         return embed
-    if not re.search(r"^\[[A-Z0-9]{3}\].*", user.display_name):
+    if not re.search(r"^\[[A-Z0-9]{3}\].*", member.display_name):
         embed = discord.Embed(title=":x: Error", color=discord.Color.red())
         embed.description = "Your nickname is not set properly. Please change your nickname to `[INITIAL] Full Name` format first."
         return embed
     else:
-        initial = re.search(r"^\[([A-Z0-9]{3})\].*", user.display_name).group(1)
+        initial = re.search(
+            r"^\[([A-Z0-9]{3})\].*", member.display_name).group(1)
+        # print(f"Assigning section roles to {initial}")
         # remove exisiting section role, if any
-        section_roles = [role for role in user.roles if role in vars.all_sec_roles]
-        await user.remove_roles(*section_roles)
+        section_roles = [
+            role for role in member.roles if role in vars.all_sec_roles]
+        await member.remove_roles(*section_roles)
+        await member.remove_roles(*vars.thoery_and_lab_faculty_roles.values())
 
-        embed = discord.Embed(title=":white_check_mark: Success", color=discord.Color.green())
+        embed = discord.Embed(
+            title=":white_check_mark: Success", color=discord.Color.green())
         embed.description = f"You have been given the following sections:\n"
 
         for ctype in literals.class_types:
-            filt = vars.df_routine[f"{ctype.title()} Teacher"].str.contains(initial)
+            filt = vars.df_routine[f"{ctype.title()} Teacher"].str.contains(
+                initial)
             ctype_sections = vars.df_routine.loc[filt, "Section"].values
-            ctype_roles = [vars.sec_roles[sec][ctype] for sec in ctype_sections]
-            all_ctype_roles = "\u200b" if not ctype_roles else ", ".join(r.mention for r in ctype_roles)
-            embed.add_field(name=f"{ctype.title()} Sections", value=all_ctype_roles)
-            await user.add_roles(*ctype_roles)
+            ctype_roles = [vars.sec_roles[sec][ctype]
+                           for sec in ctype_sections]
+            all_ctype_roles = "\u200b" if not ctype_roles else ", ".join(
+                r.mention for r in ctype_roles)
+            embed.add_field(
+                name=f"{ctype.title()} Sections", value=all_ctype_roles)
+            await member.add_roles(*ctype_roles)
+            if ctype_sections.any():
+                await member.add_roles(vars.thoery_and_lab_faculty_roles[ctype])
 
         return embed
